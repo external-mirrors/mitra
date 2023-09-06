2d20892f0b0e4d9c3ba6f842a5c94b7a62a198e0
silverpill <silverpill@firemail.cc>
2023-09-06 06:17:24 +0000

    Implement to_uri and from_uri for AccountId

--- mitra-utils/src/caip10.rs
+++ mitra-utils/src/caip10.rs
@@ -8,10 +8,31 @@ use super::caip2::{ChainId, CAIP2_RE};
 
 const CAIP10_ADDRESS_RE: &str = r"(?P<address>[-.%a-zA-Z0-9]{1,128})";
 
+// 'caip:' URI scheme has not been standardized
+// https://github.com/ChainAgnostic/CAIPs/issues/67
+const CAIP10_URI_PREFIX: &str = "caip:10:";
+
 #[derive(Clone, Debug, PartialEq)]
 pub struct AccountId {
     pub chain_id: ChainId,
     pub address: String,
+}
+
+#[derive(thiserror::Error, Debug)]
+#[error("invalid CAIP-10 ID")]
+pub struct AccountIdError;
+
+impl AccountId {
+    pub fn to_uri(&self) -> String {
+        format!("{}{}", CAIP10_URI_PREFIX, self)
+    }
+
+    pub fn from_uri(uri: &str) -> Result<Self, AccountIdError> {
+        let account_id_str = uri.strip_prefix(CAIP10_URI_PREFIX)
+            .ok_or(AccountIdError)?;
+        let account_id = account_id_str.parse()?;
+        Ok(account_id)
+    }
 }
 
 impl fmt::Display for AccountId {
@@ -19,10 +40,6 @@ impl fmt::Display for AccountId {
         write!(formatter, "{}:{}", self.chain_id, self.address)
     }
 }
-
-#[derive(thiserror::Error, Debug)]
-#[error("invalid CAIP-10 ID")]
-pub struct AccountIdError;
 
 impl FromStr for AccountId {
     type Err = AccountIdError;
@@ -59,5 +76,18 @@ mod tests {
         let value = "eip155:0xb9c5714089478a327f09197987f16f9e5d936e8a";
         let error = value.parse::<AccountId>().err().unwrap();
         assert_eq!(error.to_string(), "invalid CAIP-10 ID");
+    }
+
+    #[test]
+    fn test_account_id_uri() {
+        let value = "monero:418015bb9ae982a1975da7d79277c270:8xyz";
+        let account_id = value.parse::<AccountId>().unwrap();
+        let account_uri = account_id.to_uri();
+        assert_eq!(
+            account_uri,
+            "caip:10:monero:418015bb9ae982a1975da7d79277c270:8xyz",
+        );
+        let account_id_parsed = AccountId::from_uri(&account_uri).unwrap();
+        assert_eq!(account_id_parsed, account_id);
     }
 }
