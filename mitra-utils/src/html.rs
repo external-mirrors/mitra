0e8a4b45c3e90c68536dbc15d99244ebd130bb22
silverpill <silverpill@firemail.cc>
2023-10-30 17:39:59 +0000

    Add tests for rel=noopener insertion

--- mitra-utils/src/html.rs
+++ mitra-utils/src/html.rs
@@ -80,6 +80,13 @@ pub fn clean_html_all(html: &str) -> String {
 mod tests {
     use super::*;
 
+    fn allowed_classes() -> Vec<(&'static str, Vec<&'static str>)> {
+        vec![
+            ("a", vec!["mention", "u-url"]),
+            ("span", vec!["h-card"]),
+        ]
+    }
+
     #[test]
     fn test_clean_html() {
         let unsafe_html = concat!(
@@ -92,10 +99,18 @@ mod tests {
         );
         let safe_html = clean_html(
             unsafe_html,
-            vec![
-                ("a", vec!["mention", "u-url"]),
-                ("span", vec!["h-card"]),
-            ],
+            allowed_classes(),
+        );
+        assert_eq!(safe_html, expected_safe_html);
+    }
+
+    #[test]
+    fn test_clean_html_noopener() {
+        let unsafe_html = r#"<a href="https://external.example">link</a>"#;
+        let expected_safe_html = r#"<a href="https://external.example" rel="noopener">link</a>"#;
+        let safe_html = clean_html(
+            unsafe_html,
+            allowed_classes(),
         );
         assert_eq!(safe_html, expected_safe_html);
     }
@@ -106,12 +121,23 @@ mod tests {
         let safe_html = clean_html_strict(
             unsafe_html,
             &["a", "br", "code", "p", "span"],
-            vec![
-                ("a", vec!["mention", "u-url"]),
-                ("span", vec!["h-card"]),
-            ],
+            allowed_classes(),
         );
         assert_eq!(safe_html, r#"<p><span class="h-card"><a href="https://example.com/user" class="u-url mention" rel="noopener">@<span>user</span></a></span> test bold with a <a href="https://server.example/tag" rel="tag noopener">tag</a>, <a href="https://example.com" rel="noopener">link</a> and <code>code</code></p>"#);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_clean_html_strict_noopener() {
+        // TODO: fix cleaner
+        let unsafe_html = r#"<a href="https://external.example">link</a>"#;
+        let expected_safe_html = r#"<a href="https://external.example" rel="noopener">link</a>"#;
+        let safe_html = clean_html_strict(
+            unsafe_html,
+            &["a"],
+            allowed_classes(),
+        );
+        assert_eq!(safe_html, expected_safe_html);
     }
 
     #[test]
