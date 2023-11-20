c7c626427e110b42b61b270ce881b56371b0d035
silverpill <silverpill@firemail.cc>
2023-11-20 16:38:24 +0000

    Move AP_CONTEXT and AP_PUBLIC to mitra-activitypub::constants

--- mitra-activitypub/src/constants.rs
+++ mitra-activitypub/src/constants.rs
@@ -1,3 +1,9 @@
+// https://www.w3.org/TR/activitystreams-vocabulary/
+pub const AP_CONTEXT: &str = "https://www.w3.org/ns/activitystreams";
+
 // https://www.w3.org/TR/activitypub/#server-to-server-interactions
 pub const AP_MEDIA_TYPE: &str = r#"application/ld+json; profile="https://www.w3.org/ns/activitystreams""#;
 pub const AS_MEDIA_TYPE: &str = "application/activity+json";
+
+// https://www.w3.org/TR/activitypub/#public-addressing
+pub const AP_PUBLIC: &str = "https://www.w3.org/ns/activitystreams#Public";
--- src/activitypub/constants.rs
+++ src/activitypub/constants.rs
@@ -1,7 +1,11 @@
-pub use mitra_activitypub::constants::{AP_MEDIA_TYPE, AS_MEDIA_TYPE};
+pub use mitra_activitypub::constants::{
+    AP_CONTEXT,
+    AP_MEDIA_TYPE,
+    AP_PUBLIC,
+    AS_MEDIA_TYPE,
+};
 
 // Contexts
-pub const AP_CONTEXT: &str = "https://www.w3.org/ns/activitystreams";
 pub const W3ID_SECURITY_CONTEXT: &str = "https://w3id.org/security/v1";
 pub const W3ID_DATA_INTEGRITY_CONTEXT: &str = "https://w3id.org/security/data-integrity/v1";
 pub const W3ID_MULTIKEY_CONTEXT: &str = "https://w3id.org/security/multikey/v1";
@@ -14,7 +18,3 @@ pub const UNITS_OF_MEASURE_CONTEXT: &str = "http://www.ontology-of-units-of-meas
 // Relation types
 pub const CHAT_LINK_RELATION_TYPE: &str = "discussion";
 pub const PAYMENT_LINK_RELATION_TYPE: &str = "payment";
-
-// Misc
-// https://www.w3.org/TR/activitypub/#public-addressing
-pub const AP_PUBLIC: &str = "https://www.w3.org/ns/activitystreams#Public";
