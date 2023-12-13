7f64c62d49b1d493c2adf84e403f459bbf5b9ee6
silverpill <silverpill@firemail.cc>
2023-12-13 20:27:40 +0000

    Implement multicodec for RSA private key

--- mitra-utils/src/multicodec.rs
+++ mitra-utils/src/multicodec.rs
@@ -13,6 +13,8 @@ const MULTICODEC_ED25519_PRIV: u128 = 0x1300;
 // RSA public key. DER-encoded ASN.1 type RSAPublicKey according to IETF RFC 8017 (PKCS #1)
 // (rsa-pub)
 const MULTICODEC_RSA_PUB: u128 = 0x1205;
+// RSA private key (rsa-priv)
+const MULTICODEC_RSA_PRIV: u128 = 0x1305;
 
 fn encode(code: u128, data: &[u8]) -> Vec<u8> {
     let mut buf: [u8; 19] = Default::default();
@@ -63,6 +65,14 @@ pub fn decode_rsa_public_key(value: &[u8]) -> Result<Vec<u8>, MulticodecError> {
     decode(MULTICODEC_RSA_PUB, value)
 }
 
+pub fn encode_rsa_private_key(key: &[u8]) -> Vec<u8> {
+    encode(MULTICODEC_RSA_PRIV, key)
+}
+
+pub fn decode_rsa_private_key(value: &[u8]) -> Result<Vec<u8>, MulticodecError> {
+    decode(MULTICODEC_RSA_PRIV, value)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -89,6 +99,14 @@ mod tests {
         let value = vec![1];
         let encoded = encode_rsa_public_key(&value);
         let decoded = decode_rsa_public_key(&encoded).unwrap();
+        assert_eq!(decoded, value);
+    }
+
+    #[test]
+    fn test_rsa_priv_encode_decode() {
+        let value = vec![1];
+        let encoded = encode_rsa_private_key(&value);
+        let decoded = decode_rsa_private_key(&encoded).unwrap();
         assert_eq!(decoded, value);
     }
 }
