c1b5f9be92383b06b4eda2d2dc949380c35ca284
silverpill <silverpill@firemail.cc>
2024-10-21 00:49:04 +0000

    Add SQL query for generating remote media report

---
+++ scripts/remote_media_report.sql
@@ -0,0 +1,12 @@
+SELECT hostname, count(*), sum(file_size) / 1000000 AS size_mb
+FROM (
+    SELECT split_part(actor_profile.actor_id, '/', 3) AS hostname, file_size
+    FROM media_attachment
+    JOIN actor_profile ON actor_profile.id = media_attachment.owner_id
+    WHERE
+        actor_id IS NOT NULL
+        AND media_attachment.file_size IS NOT NULL
+        AND media_attachment.created_at > CURRENT_TIMESTAMP - INTERVAL '3 days'
+) AS media
+GROUP BY hostname
+ORDER BY size_mb DESC;
