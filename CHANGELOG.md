17e87f9327c0a3b19af20f31ea2a1e7e46f1d191
silverpill <silverpill@firemail.cc>
2025-01-22 12:29:30 +0000

    Disallow duplicated poll options

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 - Changed MSRV to 1.67.1.
 - Count physical cores when calculating database connection pool size.
+- Disallow duplicated poll options.
 
 ## [3.14.0] - 2025-01-14
 
--- mitra_validators/src/polls.rs
+++ mitra_validators/src/polls.rs
@@ -13,10 +13,36 @@ pub fn validate_poll_data(poll_data: &PollData) -> Result<(), ValidationError> {
     if poll_data.results.len() > POLL_OPTION_COUNT_MAX {
         return Err(ValidationError("too many poll options"));
     };
+    let mut unique_options = vec![];
     for result in &poll_data.results {
         if result.option_name.len() > POLL_OPTION_NAME_LENGTH_MAX {
             return Err(ValidationError("poll option name is too long"));
         };
+        if !unique_options.contains(&&result.option_name) {
+            unique_options.push(&result.option_name);
+        } else {
+            return Err(ValidationError("poll options must be unique"));
+        };
     };
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use mitra_models::polls::types::PollResult;
+    use super::*;
+
+    #[test]
+    fn test_poll_data_unique_options() {
+        let poll_data = PollData {
+            multiple_choices: false,
+            ends_at: Default::default(),
+            results: vec![PollResult::new("a"), PollResult::new("a")],
+        };
+        let result = validate_poll_data(&poll_data);
+        assert_eq!(
+            result.err().unwrap().0,
+            "poll options must be unique",
+        );
+    }
 }
