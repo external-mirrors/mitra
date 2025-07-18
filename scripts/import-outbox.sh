d96af0ff502d5422acad7ddb9af8f97a5cccf5d3
silverpill <silverpill@firemail.cc>
2025-07-18 16:05:33 +0000

    Add import-outbox script

---
+++ scripts/import-outbox.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+FILENAME="$1"
+USERNAME="$2"
+BIN='mitra'
+
+for row in $(jq -r '.orderedItems[] | select(.type == "Create") | .object | select(.inReplyTo == null) | select(.to | index("https://www.w3.org/ns/activitystreams#Public")) | @base64' ${FILENAME}); do
+    object=$(echo ${row} | base64 --decode)
+    content=$(echo ${object} | jq -r .content)
+    published=$(echo ${object} | jq -r .published)
+    ${BIN} create-post ${USERNAME} "${content}" ${published}
+done
