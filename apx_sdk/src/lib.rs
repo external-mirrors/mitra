baa077ca2eefb03ff76a7bcd844c6ea7a4593ec9
silverpill <silverpill@firemail.cc>
2026-03-31 15:10:39 +0000

    Add code examples to APx package docs

--- apx_sdk/src/lib.rs
+++ apx_sdk/src/lib.rs
@@ -17,7 +17,26 @@
 //! apx_sdk = "0.22.0"
 //! ```
 //!
-//! Examples:
+//! ## Examples
+//!
+//! Fetch an object:
+//!
+//! ```rust
+//! use apx_sdk::{
+//!     agent::FederationAgent,
+//!     fetch::{fetch_object, FetchObjectOptions},
+//! };
+//! # use apx_sdk::fetch::FetchError;
+//! # async fn run() -> Result<(), FetchError> {
+//! let object_id = "https://mastodon.social/@Mastodon";
+//! let agent = FederationAgent::default();
+//! let options = FetchObjectOptions::default();
+//! let object = fetch_object(&agent, object_id, options).await?;
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! More examples:
 //!
 //! - [FEP-ae97 client](https://codeberg.org/silverpill/mitra/src/branch/main/apx_sdk/examples/fep_ae97_client.rs)
 //! - [FEP-ae97 server](https://codeberg.org/silverpill/mitra/src/branch/main/apx_sdk/examples/fep_ae97_server.rs)
