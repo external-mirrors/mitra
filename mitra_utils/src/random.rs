55fd216a937c443fb592e46f3aa47c6945e74446
silverpill <silverpill@firemail.cc>
2026-05-17 15:12:42 +0000

    Use rand 0.9 in mitra_utils

--- mitra_utils/src/random.rs
+++ mitra_utils/src/random.rs
@@ -1,7 +1,7 @@
-use rand::Rng;
+use rand::{Rng as _};
 
 pub fn generate_random_sequence<const LEN: usize>() -> [u8; LEN] {
-    let mut rng = rand::thread_rng();
+    let mut rng = rand::rng();
     let mut value = [0u8; LEN];
     rng.fill(&mut value[..]);
     value
