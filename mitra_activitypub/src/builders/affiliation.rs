b76d982132ecee2018fa02683732ee839e6d6c1d
silverpill <silverpill@firemail.cc>
2026-07-28 12:22:26 +0000

    Add test for `Affiliation::new`

--- mitra_activitypub/src/builders/affiliation.rs
+++ mitra_activitypub/src/builders/affiliation.rs
@@ -1,9 +1,11 @@
 use apx_sdk::core::url::canonical::NonCanonicalUri;
 use serde::Serialize;
 
-use mitra_models::relationships::types::{
-    RelatedActorProfile,
-    RelationshipType,
+use mitra_models::{
+    relationships::types::{
+        RelatedActorProfile,
+        RelationshipType,
+    },
 };
 
 use crate::{
@@ -25,7 +27,7 @@ impl Affiliation {
         authority: &Authority,
         related_profile: &RelatedActorProfile<i32>,
     ) -> Self {
-        let id_builder = IdBuilder::for_profile(
+        let subject_id_builder = IdBuilder::for_profile(
             authority,
             &related_profile.profile,
         );
@@ -35,11 +37,39 @@ impl Affiliation {
         );
         Self {
             object_type: RELATIONSHIP,
-            subject: id_builder.build_unchecked(&canonical_actor_id),
+            subject: subject_id_builder.build_unchecked(&canonical_actor_id),
             relationship: match related_profile.relationship_type {
                 RelationshipType::GroupAdmin => "admin",
                 _ => "none",
             },
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use mitra_models::profiles::types::DbActorProfile;
+    use super::*;
+
+    #[test]
+    fn test_build_affiliation() {
+        let authority = Authority::server_unchecked("https://social.example");
+        let user = DbActorProfile::local_for_test("test");
+        let related_user = RelatedActorProfile {
+            related_id: 1,
+            relationship_type: RelationshipType::GroupAdmin,
+            profile: user,
+        };
+        let affiliation = Affiliation::new(
+            &authority,
+            &related_user,
+        );
+        let value = serde_json::to_value(affiliation).unwrap();
+        let expected_value = serde_json::json!({
+            "type": "Relationship",
+            "subject": "https://social.example/users/test",
+            "relationship": "admin",
+        });
+        assert_eq!(value, expected_value);
     }
 }
