d285069708a007d7e33a911230918fd5994d7259
silverpill <silverpill@firemail.cc>
2026-08-22 13:02:57 +0000

    Add `media fetch` command

--- mitra_cli/src/commands/media.rs
+++ mitra_cli/src/commands/media.rs
@@ -1,9 +1,11 @@
 use anyhow::Error;
+use apx_sdk::fetch::fetch_media;
 use clap::{
     Parser,
     Subcommand,
 };
 
+use mitra_activitypub::agent::build_federation_agent;
 use mitra_adapters::media::delete_files;
 use mitra_config::Config;
 use mitra_models::{
@@ -11,6 +13,31 @@ use mitra_models::{
     media::queries::{find_orphaned_files, get_local_files},
 };
 use mitra_services::media::MediaStorage;
+
+/// Fetch file and save it to the media directory
+#[derive(Parser)]
+pub struct FetchMedia {
+    url: String,
+}
+
+impl FetchMedia {
+    pub async fn execute(
+        self,
+        config: &Config,
+    ) -> Result<(), Error> {
+        let agent = build_federation_agent(&config.instance(), None);
+        let media_storage = MediaStorage::new(config);
+        let (file_data, media_type) = fetch_media(
+            &agent,
+            &self.url,
+            &config.limits.media.supported_media_types(),
+            config.limits.media.file_size_limit,
+        ).await?;
+        let file_info = media_storage.save_file(file_data, &media_type)?;
+        println!("file saved: {}", file_info.file_name);
+        Ok(())
+    }
+}
 
 /// List files uploaded by local users
 #[derive(Parser)]
@@ -68,6 +95,7 @@ impl DeleteOrphanedFiles {
 /// Manage media
 #[derive(Subcommand)]
 pub enum MediaCommand {
+    Fetch(FetchMedia),
     Local(ListLocalFiles),
     DeleteOrphaned(DeleteOrphanedFiles),
 }
@@ -79,6 +107,7 @@ impl MediaCommand {
         db_pool: &DatabaseConnectionPool,
     ) -> Result<(), Error> {
         match self {
+            Self::Fetch(command) => command.execute(config).await,
             Self::Local(command) => command.execute(config, db_pool).await,
             Self::DeleteOrphaned(command) => command.execute(config, db_pool).await,
         }
